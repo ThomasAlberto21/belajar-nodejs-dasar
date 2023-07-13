a12ee23a5d1b6d3b8f344fd934df16c995c0bd52
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-13 19:35:27 +0700

    os

---
+++ os/os.mjs
@@ -0,0 +1,16 @@
+import os from "os"
+
+console.info(os.platform())
+console.info(os.arch())
+console.info(os.cpus())
+console.info(os.freemem())
+console.info(os.totalmem())
+console.info(os.homedir())
+console.info(os.hostname())
+console.info(os.networkInterfaces())
+console.info(os.release())
+console.info(os.tmpdir())
+console.info(os.type())
+console.info(os.uptime())
+console.info(os.userInfo())
+console.info(os.version())
